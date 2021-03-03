1bf787884adf10a2db2b6af9b1066f3f5b0ed234
DHN Chandan <php.chandan@gmail.com>
2021-03-03 22:53:17 +0600

    chore: :heavy_plus_sign: Add required aws cdk dependencies

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,4 +1,8 @@
 import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as rds from "@aws-cdk/aws-rds";
 
 export class CdkStack extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
